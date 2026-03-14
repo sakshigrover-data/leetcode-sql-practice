a44d01d18e8134daf4d2c6ed5efb22e88a65df44
Sakshi Grover <sakshigroverncr2@gmail.com>
2026-03-14 12:47:19 +0530

    Update recyclable-and-low-fat-products.sql

--- easy/recyclable-and-low-fat-products.sql
+++ easy/recyclable-and-low-fat-products.sql
@@ -1,7 +1,7 @@
--- LeetCode problem 1757
--- Recyclable and Low Fat Products
+-- LeetCode problem 1757 - Recyclable and Low Fat Products
+-- Concept: WHERE + AND
 
 SELECT product_id
 FROM Products
-WHERE low_fats = 'Y' and 
+WHERE low_fats = 'Y' AND 
       recyclable = 'Y';
